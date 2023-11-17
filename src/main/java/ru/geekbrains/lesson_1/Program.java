561d8a12f9214810442f07c0c3d487cb2bfd4384
igor-i.s1@ya.ru <igor-i.s1@ya.ru>
2023-11-17 14:30:13 +0300

    Семинар 1

--- src/main/java/ru/geekbrains/lesson_1/Program.java
+++ src/main/java/ru/geekbrains/lesson_1/Program.java
@@ -1,14 +1,43 @@
 package ru.geekbrains.lesson_1;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i ++) {
-            if(i % 2 == 0) {
-                continue;
-            }
-            System.out.println(i);
-        }
+    printName();
+    task2();
     }
+    /*
+    * Написать программу, которая запросит
+    * пользователя ввести "Имя" в консоли.
+    * Получит введеную строку и выведет в консоль сообщение "Привет, "Имя!".
+     */
+    static void printName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя пользоватлея: ");
+        String name = scanner.nextLine();
+        System.out.println("Привет, " + name + "!");
+        System.out.printf("Привет, %s !\n",name);
+    }
+    static void task2(){
+        /*
+        Дан массив двоичных чискл, например [1, 1, 0, 1, 1, 1, 0, 1],
+        вывести максимальное количество подряд илущих 1.
+         */
+        int [] arr = new int[] {1, 1, 0, 1, 1, 1, 0, 1};
+        int max = 0;
+        int num = 0;
+        for(int i : arr){
+            if(i == 1){
+                num++;}
+            else {
+                if(max < num){
+                    max = num;
+                    num = 0;
+                }
+                }
+            }
+        System.out.printf("Максимальное количество подряд идущих 1 равно: %d", max);
+        }
     }
