b36440f07629d1e1ee5270a277c4b4d9194e0964
igor-i.s1@ya.ru <igor-i.s1@ya.ru>
2023-11-17 08:25:34 +0300

    Сохранение

--- src/main/java/ru/geekbrains/lesson_1/Program.java
+++ src/main/java/ru/geekbrains/lesson_1/Program.java
@@ -4,34 +4,11 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        task2();
-    }
-    static void printName(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите ваше имя: ");
-        String name = scanner.nextLine();
-        System.out.println("Привет, " + name + "!");
-        System.out.printf("Привет, %s !\n",name);
-    }
-
-    /*
-    * Дан массив данных чисел, например [1, 1, 0, 1, 1, 1, 0,1 ]
-    * вывести ммксимальное количество идущих подряд 1.
-     */
-    static void task2(){
-        int [] arr = new int[] {1, 1, 0, 1, 1, 1, 0, 1} ;
-        int count1 = 0;
-        int count2 = 0;
-        for (int j : arr) {
-            if (j == 1) {
-                count1 += 1;
+        for (int i = 0; i < 10; i ++) {
+            if(i % 2 == 0) {
+                continue;
             }
-            if (count2 < count1) {
-                count2 = count1;
-                count1 = 0;
-            }
-
+            System.out.println(i);
         }
-        System.out.printf("Максимальное количество подряд идущих единиц составляет: %d\n", count2);
     }
-}
+    }
