526767c1fbd164783b962e8d8f2996ba1b857801
igor-i.s1@ya.ru <igor-i.s1@ya.ru>
2023-11-22 16:53:39 +0300

    Задача 2

---
+++ src/main/java/HomeWork/Lesson_1/Task_1/Task2.java
@@ -0,0 +1,29 @@
+package HomeWork.Lesson_1.Task_1;
+
+
+//Вывести все простые числа от 1 до 1000 (простое числе делится толкьо на 1 и на себя)
+public class Task2 {
+    public static void main(String[] args) {
+    simpleNumber();
+    }
+
+    public static void simpleNumber() {
+//        int[] arr = new int[];
+//        int count = 0;
+//        int num;
+
+        for(int i = 2; i <= 1000; i ++) {
+            for(int j = 2; j <= i; j ++) {
+                if(i % j != 0) {
+                    j++;}
+                else {
+                    System.out.println(i + " ");
+                    break;}
+
+
+
+            }
+
+        }
+    }
+}
